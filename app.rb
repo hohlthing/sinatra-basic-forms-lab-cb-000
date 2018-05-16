a66ab93d95dc41be694606b88bc19ca55dd12e86
michael hohl <mhohl@rocketmail.com>
2018-05-16 21:04:54 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,5 +6,14 @@ class App < Sinatra::Base
     erb :index
   end
 
+  get '/new' do
+    erb :create_puppy
+  end
+
+  post '/' do
+    @puppy = Puppy.new(params[:name].to_s, params[:breed].to_s, params[:age].to_s)
+    erb :display_puppy
+  end
+
 
 end
